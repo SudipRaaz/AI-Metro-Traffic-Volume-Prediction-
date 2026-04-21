c8e1d82163d21b3dcb4a5b09ac99ff39ae78deed
unknown <subhamshrestha500@gmail.com>
2026-04-21 23:07:49 +0930

    Refactor

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from ucimlrepo import fetch_ucirepo 
 from preprossing import preprocess
-from sklearn.ensemble import RandomForestRegressor
-import numpy as np
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
-from sklearn.model_selection import GridSearchCV
+from model import train
+from evulate import evaluate_model
 # fetch dataset 
 metro_interstate_traffic_volume = fetch_ucirepo(id=492) 
 
@@ -27,45 +24,10 @@ X_train, y_train, X_val, y_val, X_test, y_test, scaler = preprocess()
 
 # train model
 
+y_pred = train(X_train,y_train,X_test)
 
-model = RandomForestRegressor(
-    n_estimators=100,
-    max_depth=15,
-    random_state=42,
-    n_jobs=-1
-)
-
-model.fit(X_train, y_train)
-
-param_grid = {
-    'n_estimators': [100, 200],
-    'max_depth': [10, 15, 20],
-    'min_samples_split': [2, 5]
-}
-
-grid = GridSearchCV(
-    RandomForestRegressor(random_state=42),
-    param_grid,
-    cv=3,
-    scoring='neg_mean_squared_error',
-    n_jobs=-1
-)
-
-grid.fit(X_train, y_train)
-
-best_model = grid.best_estimator_
-
-
-y_pred = best_model.predict(X_test)
-
-mse = mean_squared_error(y_test, y_pred)
-rmse = np.sqrt(mse)
-mae = mean_absolute_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
-
-print("RMSE:", rmse)
-print("MAE:", mae)
-print("R² Score:", r2)
+#Evaluate results
+evaluate_model(y_pred, X_test, y_test)
 
 
   
--- model.py
+++ model.py
@@ -1,49 +1,41 @@
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 
-model = RandomForestRegressor(
+def train(X_train,y_train,X_test):
+    model = RandomForestRegressor(
     n_estimators=100,
     max_depth=15,
     random_state=42,
     n_jobs=-1
-)
+            )
 
-model.fit(X_train, y_train)
+    model.fit(X_train, y_train)
 
-from sklearn.model_selection import GridSearchCV
-
-param_grid = {
+    param_grid = {
     'n_estimators': [100, 200],
     'max_depth': [10, 15, 20],
     'min_samples_split': [2, 5]
-}
+    }
 
-grid = GridSearchCV(
-    RandomForestRegressor(random_state=42),
-    param_grid,
-    cv=3,
-    scoring='neg_mean_squared_error',
-    n_jobs=-1
-)
+    grid = GridSearchCV(
+        RandomForestRegressor(random_state=42),
+        param_grid,
+        cv=3,
+        scoring='neg_mean_squared_error',
+        n_jobs=-1
+    )
 
-grid.fit(X_train, y_train)
+    grid.fit(X_train, y_train)
 
-best_model = grid.best_estimator_
-
-
-
-
+    best_model = grid.best_estimator_
+    y_pred = best_model.predict(X_test)
+    return y_pred
 
 
 
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-y_pred = best_model.predict(X_test)
 
-mse = mean_squared_error(y_test, y_pred)
-rmse = np.sqrt(mse)
-mae = mean_absolute_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
 
-print("RMSE:", rmse)
-print("MAE:", mae)
-print("R² Score:", r2)
+
+
+
